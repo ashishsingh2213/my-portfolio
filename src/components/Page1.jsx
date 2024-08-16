8f811845977e277efbf09e7c7ab038b0161c3d14
ashishsingh2213 <ashishsingh2213@gmail.com>
2024-08-16 22:42:13 +0530

    react-protfoloio

--- src/components/Page1.jsx
+++ src/components/Page1.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ashii from '../image/profile.jpeg'; // Corrected import syntax
 
 const Page1 = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate('/contact');
+  };
+
   const handleDownloadResume = () => {
-    // Trigger the download by navigating to the file URL
     window.open('/resume.pdf', '_blank', 'noopener,noreferrer');
   };
+
   return (
     <div className="page1" id='page1'>
       <div className="left-page1">
@@ -17,27 +23,27 @@ const Page1 = () => {
           <p>Hello, I’m Ashish, I support designers and agencies with creative development</p>
         </div>
         <div className="button-info1">
-          <button to="/contact" className="btn1">Let's Talk</button>
-          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer"></a>
-          <button to="/resume" className="btn2" onClick={handleDownloadResume}>View Resume</button>
+          <button className="btn1" onClick={handleNavigate}>Let's Talk</button>
+          <button className="btn2" onClick={handleDownloadResume}>View Resume</button>
+
         </div>
       </div>
       <div className="right-page1">
         <div className="profile-img-info">
-          <img src={ashii} alt="Ashish Profile" /> {/* Added alt attribute */}
+          <img src={ashii} alt="Ashish Profile" />
           <div className="social-link">
-            <Link to="https://www.instagram.com/123/" target='_blank' className="social-icon">
+            <a href="https://www.instagram.com/123/" target='_blank' rel='noopener noreferrer' className="social-icon">
               <i className="ri-instagram-line"></i>
-            </Link>
-            <Link to="https://www.linkedin.com/in/ashish-singh-a703692ba/" target='_blank' className="social-icon">
+            </a>
+            <a href="https://www.linkedin.com/in/ashish-singh-a703692ba/" target='_blank' rel='noopener noreferrer' className="social-icon">
               <i className="ri-linkedin-fill"></i>
-            </Link>
-            <Link to="https://github.com/ashishsingh2213" target='_blank' className="social-icon">
+            </a>
+            <a href="https://github.com/ashishsingh2213" target='_blank' rel='noopener noreferrer' className="social-icon">
               <i className="ri-github-fill"></i>
-            </Link>
-            <Link to="https://x.com/ashishs40385576?t=MerjTlJbaiIQmscXWpq6UQ&s=09" target='_blank' className="social-icon">
+            </a>
+            <a href="https://x.com/ashishs40385576?t=MerjTlJbaiIQmscXWpq6UQ&s=09" target='_blank' rel='noopener noreferrer' className="social-icon">
               <i className="ri-twitter-x-line"></i>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
